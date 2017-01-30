095c24fc0d7025f57b9e28eeb38403e3a150603b
Kaz Matthews and Nicole Sanders <nicole@>
2017-01-30 15:49:13 -0800

    add objects, constructor, and arrays for cells.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+function Cell(bomb) {
+  this.isBomb = bomb;
+  this.adjValue = 0;
+}
+
+var a0 = new Cell(false);
+var a1 = new Cell(false);
+var a2 = new Cell(true);
+var b0 = new Cell(false);
+var b1 = new Cell(true);
+var b2 = new Cell(false);
+var c0 = new Cell(false);
+var c1 = new Cell(false);
+var c2 = new Cell(false);
+
+var a = [a0, a1, a2];
+var b = [b0, b1, b2];
+var c = [c0, c1, c2];
+var gameRows = [a, b, c];
+var width = a.length;
 
 $(document).ready(function() {
   var stateOfGame = true;
