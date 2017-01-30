f2c50acd82b8c8c06fb6e656764cbf81d97994b3
Kaz Matthews and Nicole Sanders <nicole@>
2017-01-30 15:00:30 -0800

    add if else that makes it impossible to click anything once game is over.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,30 @@
+
 $(document).ready(function() {
+  var stateOfGame = true;
   $(".cell").mousedown(function(event) {
-    switch (event.which) {
-      case 1:
-      if ($(this).hasClass("has-bomb")){
-        $(".has-bomb").addClass("bomb-clicked");
-      } else {
-        $(this).addClass("clicked-on");
+    if (stateOfGame === true) {
+      switch (event.which) {
+        case 1:
+        if ($(this).hasClass("has-bomb")){
+          $(".has-bomb").addClass("bomb-clicked");
+          stateOfGame = false;
+          console.log(stateOfGame);
+        } else {
+          $(this).addClass("clicked-on");
+        }
+        break;
+
+        case 3:
+        if (!$(this).hasClass("clicked-on")){
+          $(this).addClass("flag");
+        }
+        if ($(this).hasClass("flag")) {
+          $(this).removeClass("flag")
+        } else {
+          $(this).addClass("flag");
+        }
       }
-      break;
-      case 3:
-      <<<<<<< HEAD
-      if (!$(this).hasClass("clicked-on")){
-        $(this).addClass("flag");
-      } else if ($(this).hasClass("flag")) {
-        $(this).removeClass("flag")
-      } else {
-        $(this).addClass("flag");
-      }
-      >>>>>>> 47a8369b59f8aec09ba8c3e60ba68d22e03615ee
     }
-  })
+  });
+
 })
