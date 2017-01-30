2290804754bbc82d35d03206b38236f6879359ab
Kaz Matthews and Nicole Sanders <nicole@>
2017-01-30 13:59:54 -0800

    add a mousedown event handler where a left click adds a bombclicked or clickedon class and a right click adds a flag; a flag class was styled in css with an orange background

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
-  $(".cell").click(function() {
-    if ($(this).hasClass("has-bomb")){
-      $(this).addClass("bomb-clicked");
-    } else {
-      $(this).addClass("clicked-on");
+  $(".cell").mousedown(function(event) {
+    switch (event.which) {
+      case 1:
+        if ($(this).hasClass("has-bomb")){
+          $(this).addClass("bomb-clicked");
+        } else {
+          $(this).addClass("clicked-on");
+        }
+        break;
+      case 3:
+        $(this).addClass("flag");
     }
   })
 })
