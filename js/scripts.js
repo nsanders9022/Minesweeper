71565255b3ee822bf8672bbdb1124fcf1fed5917
Cassie Musolf and Kaz Matthews <kaz@>
2017-01-31 10:18:38 -0800

    add function that makes arrayfull of surrounding cells.

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,10 @@ function Cell(bomb, id) {
   this.bombsTouching = 1;
 }
 
-<<<<<<< HEAD
+
 var a0 = new Cell(false, 0);
 var a1 = new Cell(false, 1);
 var a2 = new Cell(false, 2);
-=======
-var a0 = new Cell(false, "0");
-var a1 = new Cell(false, "1");
-var a2 = new Cell(true, "2");
->>>>>>> master
 var b0 = new Cell(false, 3);
 var b1 = new Cell(true, 4);
 var b2 = new Cell(false, 5);
@@ -24,31 +19,32 @@ var c1 = new Cell(false, 7);
 var c2 = new Cell(false, 8);
 var cellArray = [a0, a1, a2, b0, b1, b2, c0, c1, c2];
 
-<<<<<<< HEAD
+
 function cellIsBomb(obj) {
   return obj.isBomb == true;
 }
 var bomb = cellArray.filter(cellIsBomb);
 var bombCellId = bomb[0].cellId;
-=======
-var newArray = [a0, a1, a2, b0, b1, b2, c0, c1, c2];
+var touchingCells = [3, 5, 1, 7];
 
+// function west(obj) {
+//   var surroundingCells= [];
+//   surroundingCells.push(obj.cellId == bombCellId-1);
+//   return surroundingCells;
+// }
 
-function west(obj) {
-  var surroundingCells= [];
-  surroundingCells.push(obj.cellId == bombCellId-1);
-  return surroundingCells;
+var surroundingCells = function() {
+  var array = [];
+  for (i = 0; i < cellArray.length; i++) {
+    for (j = 0; j < touchingCells.length; j++) {
+      if(cellArray[i].cellId === touchingCells[j]) {
+        array.push(cellArray[i]);
+      }
+    }
+  }
+  return array;
 }
-var westCell = cellArray.filter(west);
 //
-// function west(obj) {
-//   return obj.cellId == bombCellId-1;
-// }
-// var westCell = cellArray.filter(west);
-//
-// function west(obj) {
-//   return obj.cellId == bombCellId-1;
-// }
 // var westCell = cellArray.filter(west);
 // westCell[0].adjValue += 1;
 
@@ -63,13 +59,8 @@ var westCell = cellArray.filter(west);
 //   return obj.cellId == 4;
 // });
 
-
-
 $(document).ready(function() {
-<<<<<<< HEAD
-  console.log(westCell);
-=======
->>>>>>> master
+  console.log(surroundingCells());
   var stateOfGame = true;
   $(".minesweeper-game").contextmenu(function() {
     return false;
