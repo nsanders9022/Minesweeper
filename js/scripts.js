335dd2004a2116f0150d633b04cf56293296aaba
Cassie Musolf and Kaz Matthews <cassie@>
2017-01-31 14:08:17 -0800

    add if else statements to splice array of cells that are touching bombs so that bombs can be in corners and on sides.

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,15 @@ function Cell(bomb, id) {
   this.bombsTouching = 1;
 }
 
-var a0 = new Cell(false, 0);
+var a0 = new Cell(true, 0);
 var a1 = new Cell(false, 1);
-var a2 = new Cell(false, 2);
+var a2 = new Cell(true, 2);
 var b0 = new Cell(false, 3);
-var b1 = new Cell(true, 4);
+var b1 = new Cell(false, 4);
 var b2 = new Cell(false, 5);
-var c0 = new Cell(false, 6);
+var c0 = new Cell(true, 6);
 var c1 = new Cell(false, 7);
-var c2 = new Cell(false, 8);
+var c2 = new Cell(true, 8);
 var cellArray = [a0, a1, a2, b0, b1, b2, c0, c1, c2];
 
 function cellIsBomb(obj) {
@@ -40,15 +40,54 @@ var touch = function() {
   var allCells = [];
   for (l = 0; l < bId.length; l++) {
     var x = bId[l];
-    allCells.push(bId[l] - base);
-    allCells.push(bId[l] + base);
-    allCells.push(bId[l] +1);
-    allCells.push(bId[l] -1);
-    allCells.push(bId[l] - (base + 1));
-    allCells.push(bId[l] + (base+1));
-    allCells.push(bId[l] - (base-1));
-    allCells.push(bId[l] + (base-1));
+    var nw = x - (base + 1);
+    var n = x - base;
+    var ne = x - (base - 1);
+    var w = x - 1;
+    var e = x + 1;
+    var sw = x + (base - 1);
+    var s = x + base;
+    var se = x + (base + 1);
+    allCells.push(nw);
+    allCells.push(n);
+    allCells.push(ne);
+    allCells.push(w);
+    allCells.push(e);
+    allCells.push(sw);
+    allCells.push(s);
+    allCells.push(se);
+    console.log(allCells);
+    if (x < base) {
+      allCells.splice(allCells.indexOf(nw), 1);
+      allCells.splice(allCells.indexOf(n), 1);
+      allCells.splice(allCells.indexOf(ne), 1);
+      console.log(allCells);
     }
+    if (x >= base * (base - 1)) {
+      allCells.splice(allCells.indexOf(sw), 1);
+      allCells.splice(allCells.indexOf(s), 1);
+      allCells.splice(allCells.indexOf(se), 1);
+    }
+    if (x % base === base - 1) {
+      if (allCells.indexOf(ne) !== -1) {
+        allCells.splice(allCells.indexOf(ne), 1);
+      }
+      allCells.splice(allCells.indexOf(e), 1);
+      if (allCells.indexOf(se) !== -1) {
+        allCells.splice(allCells.indexOf(se), 1);
+      }
+      console.log(allCells);
+    }
+    if (x % base === 0) {
+      if (allCells.indexOf(nw) !== -1) {
+        allCells.splice(allCells.indexOf(nw), 1);
+      }
+      allCells.splice(allCells.indexOf(w), 1);
+      if (allCells.indexOf(sw) !== -1) {
+        allCells.splice(allCells.indexOf(sw), 1);
+      }
+    }
+  }
   return allCells;
 }
 var touchCells = touch();
@@ -67,9 +106,9 @@ var surroundingCells = function() {
 }
 
 $(document).ready(function() {
+  console.log(touchCells);
   var array = surroundingCells();
   console.log(array);
-  surroundingCells();
   for (i = 0; i < cellArray.length; i++) {
     if (cellArray[i].isBomb) {
       $("#" + cellArray[i].cellId).addClass("has-bomb");
