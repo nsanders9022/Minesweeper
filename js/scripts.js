bf943d7ac19ef6ebbad3e1e4bc39436a9c988cfd
Cassie Musolf and Kaz Matthews <cassie@>
2017-01-31 09:32:38 -0800

    Make functions for surrounding cells

--- js/scripts.js
+++ js/scripts.js
@@ -2,41 +2,59 @@ function Cell(bomb, id) {
   this.isBomb = bomb;
   this.adjValue = 0;
   this.cellId = id;
+  this.bombsTouching = 1;
 }
 
 var a0 = new Cell(false, 0);
 var a1 = new Cell(false, 1);
-var a2 = new Cell(true, 2);
+var a2 = new Cell(false, 2);
 var b0 = new Cell(false, 3);
 var b1 = new Cell(true, 4);
 var b2 = new Cell(false, 5);
 var c0 = new Cell(false, 6);
 var c1 = new Cell(false, 7);
 var c2 = new Cell(false, 8);
+var cellArray = [a0, a1, a2, b0, b1, b2, c0, c1, c2];
 
-var newArray = [a0, a1, a2, b0, b1, b2, c0, c1, c2];
+function cellIsBomb(obj) {
+  return obj.isBomb == true;
+}
+var bomb = cellArray.filter(cellIsBomb);
+var bombCellId = bomb[0].cellId;
 
-var a = [a0, a1, a2];
-var b = [b0, b1, b2];
-var c = [c0, c1, c2];
-var gameRows = [a, b, c];
-var width = a.length;
-
-var touching = newArray.filter(function(obj){
-  return obj.cellId == $(this).attr("id");
-});
-
-// Cell.prototype.getId = function() {
-//   var idCell = this.cellId;
+function west(obj) {
+  var surroundingCells= [];
+  surroundingCells.push(obj.cellId == bombCellId-1);
+  return surroundingCells;
+}
+var westCell = cellArray.filter(west);
 //
+// function west(obj) {
+//   return obj.cellId == bombCellId-1;
 // }
+// var westCell = cellArray.filter(west);
+//
+// function west(obj) {
+//   return obj.cellId == bombCellId-1;
+// }
+// var westCell = cellArray.filter(west);
+// westCell[0].adjValue += 1;
 
-// Cell.prototype.whereDaBombs = function() {
-//   return this.adjValue;
-// };
+//
+// var west = cellArray.filter(function(obj){
+//   return obj.cellId == bombCellId-1;
+//   return obj.cellId == bombCellId+1;
+// });
+
+
+// var touching = cellArray.filter(function(obj){
+//   return obj.cellId == 4;
+// });
+
+
 
 $(document).ready(function() {
-  console.log(touching);
+  console.log(westCell);
   var stateOfGame = true;
   $(".minesweeper-game").contextmenu(function() {
     return false;
@@ -69,3 +87,9 @@ $(document).ready(function() {
 
   });
 });
+
+// var a = [a0, a1, a2];
+// var b = [b0, b1, b2];
+// var c = [c0, c1, c2];
+// var gameRows = [a, b, c];
+// var width = a.length;
